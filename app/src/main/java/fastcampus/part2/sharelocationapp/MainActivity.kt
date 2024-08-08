1c6897e0fdfa62480f8ff25efc565776514e4792
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-08-08 17:52:05 +0900

    뷰 바인딩 활성화

--- app/src/main/java/fastcampus/part2/sharelocationapp/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/sharelocationapp/MainActivity.kt
@@ -3,10 +3,15 @@ package fastcampus.part2.sharelocationapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import fastcampus.part2.sharelocationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
